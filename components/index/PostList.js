da71935d2788cef1580be3740790ac5ec96b2f8c
kazuki-nishimura <julionadtoni7@yahoo.ne.jp>
2022-03-06 07:30:47 +0900

    [change] change index.js along with design

--- components/index/PostList.js
+++ components/index/PostList.js
@@ -1,32 +1,65 @@
-import Image from "next/image";
 import styles from "../../styles/index/PostList.module.css";
+import { useState } from "react";
 
 const PostList = props => {
-  const posts = props.posts;
-  const image_width = 250;
+  // const posts = props.posts;
+  const posts = [...Array(20)].map((_, i) => i); //TODO delete
 
-  const comment = "アルパカ食べたいアルパカ食べたいアルパカ食べたい"
-  const sentTo = "医療従事者";
+  const [readMoreCount, setReadMoreCount] = useState(1);
+  const [cantReadMore, setCantReadMore] = useState(false);
+  const num = 6; //* 一回で表示されるpostの数
+  const readMoreBtnClicked = event => {
+    event.preventDefault();
+    setReadMoreCount( prevCount => {
+      const newCount = prevCount + 1;
+      if (posts.length/num < newCount) setCantReadMore(true);
+      return newCount;
+    } );
+  }
+
+  // TODO delete
+  const comment = "アルパカ食べたいアルパカ食べたいアルパカ食べたいアルパカ食べたいアルパカ食べたいアルパカ食べたいアルパカ食べたいアルパカ食べたいアルパカ食べたい"
+  const post_list = [
+    {sentTo_1: "医療従事者", sentTo_2: "の方", message: "いつも私たちのために最前線で働いてくださり、ありがとうございます！"},
+    {sentTo_1: "コロナに", sentTo_2: "感染した方", message: "はやく治りますように！"},
+    {sentTo_1: "飲食店に", sentTo_2: "勤務の方", message: "いつも美味しいご飯をありがとうございます。"},
+    {sentTo_1: "アルパカ", sentTo_2: "の方", message: comment},
+  ]
+  const shown_post = posts.slice(0, readMoreCount*num);
+  console.log(shown_post);
 
   return (
-    <ul className={styles.container}>
-      {posts.map( post => {
-        return (
-          <li key={post.id}>
-            <div>
-              <span>{sentTo}</span>の方へ
-            </div>
-            <Image
-              src={post.webformatURL}
-              alt={post.tags}
-              width={image_width}
-              height={post.webformatHeight*image_width/post.webformatWidth}
-            />
-            <p>{comment}</p>
-          </li>
-        )
-      })}
-    </ul>
+    <div className={styles.container}>
+      <h2 className="shelby">Messages</h2>
+      <ul>
+        {shown_post.map( num => {
+          //TODO Delete these lines -------
+          const i = Math.floor(Math.random() * post_list.length);
+          const post = post_list[i];
+          post.id = num;
+          //TODO---------------------------
+          return (
+            <li key={post.id}>
+              <span className="shelby">Dear</span>
+              <h3 className="flower-butterfly">
+                {post.sentTo_1}<br />{post.sentTo_2}
+              </h3>
+              <p>{post.message}</p>
+            </li>
+          )
+        })}
+      </ul>
+      {!cantReadMore && (
+        <div align="right">
+          <button
+            className="flower-butterfly"
+            onClick={readMoreBtnClicked}
+          >
+            もっと見る
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
--- components/index/StoryList.js
+++ components/index/StoryList.js
@@ -1,29 +1,27 @@
 import styles from "../../styles/index/StoryList.module.css";
-import Image from "next/image";
 
 const StoryList = props => {
-  const posts = props.posts;
-  const src = "https://cdn.pixabay.com/user/2022/03/02/14-36-31-529_250x250.jpg";
-  const image_width = 100;
-  const sentTo = "医療従事者";
+  // const posts = props.posts;
+  const story_list = [
+    {id: 1, sentTo_1: "医療従事者の", sentTo_2: "皆さん", img: "doctor"},
+    {id: 2, sentTo_1: "コロナに", sentTo_2: "感染した皆さん", img: "doctor"},
+    {id: 3, sentTo_1: "飲食店で", sentTo_2: "働く皆さん", img: "doctor"},
+  ]
   return (
     <div className={styles.container}>
-      <ul>
-        {posts.map( post => {
-          return (
-            <li key={post.id}>
-              <div>
-                <Image
-                  src={src}
-                  width={image_width}
-                  height={image_width}
-                />
-              </div>
-              <p><span>{sentTo}</span>の方へ</p>
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <h2 className="shelby">Digest</h2>
+        <ul>
+          {story_list.map( story => {
+            return (
+              <li key={story.id} className={story.img}>
+                <p>{story.sentTo_1}</p>
+                <p>{story.sentTo_2}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
 
   )
