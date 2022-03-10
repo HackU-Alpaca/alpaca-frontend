6960817e55c1957aae31b53232bc062dc7d233a3
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2022-03-11 00:20:01 +0900

    [Feature/23] モーダル画面上のpost遷移ボタンの実装 (#24)
    * [add] #1 nextjsの雛形を作成
    
    * [add] create layout-related js/css files
    
    * [add] create index.js & list component
    
    * [add] introduce PixaBay api for checking of functions
    
    * [add] create an index-related css file
    
    * [update] change from useEffect to useSWR
    
    * [change] change the name of list component to 'PostList'
    
    * [add] create insta-story-like component/css
    
    * [change] change the name of css file along with components
    
    * [add] add fonts and add-icon
    
    * [change] change index.js along with design
    
    * [add] add a modal window
    
    * [add] add a story-like digest function
    
    * [fix] fix a memory leak problem
    
    * [change] make a minor change
    
    * Revert "[fix] fix a memory leak problem"
    
    This reverts commit 29410d18e9691ea16f616a93f50f8fadc4bde59e.
    
    * [change] change .gitignore a bit
    
    * [add] add a post modal component
    
    * [update] devide data fetching from post list component
    
    * [update] divide data fetching from digest list component
    
    * [update] update styles of the post modal
    
    * [remove] delete fonts in remote repository
    
    * [change] divide layouts between Index.js and others
    
    * [add] create a read more modal
    
    * [change] divide stylesheets of modals
    
    * [add] add a modal of the add-post description page
    
    * [remove] delete unnecessary files
    
    * [update] arrange data fetching
    
    * [add] add a sort icon and style
    
    * [add] add a search icon and style
    
    * [update] update the QR code and link
    
    * [add] add a sort function by tags
    
    * [remove] delete unnecessary css file
    
    * [add] add a search function
    
    * [add] add a time-sort function
    
    * [add] gather sort functions
    
    * [add] add new sort icons
    
    * [update] connect Enter key to a submit action of input
    
    * [fix] fix a minor bug
    
    * [fix] fix minor bugs
    
    * [update] update index styles
    
    * [add] add a skeleton screen while loading
    
    * [add] add SWR handlings and an error component
    
    * [fix] fix a minor bug
    
    * [update] gather modal components to index.js of a modal dir
    
    * [fix] fix a minor bug
    
    * [add] add styles of an error page
    
    * [add] add error data sets
    
    * [fix] fix a minor bug
    
    * [fix] fix a minor bug
    
    * [update] write an instrunction for starting app
    
    * [fix] fix modal sizes
    
    * [add] add shift buttons in a post modal
    
    Co-authored-by: kazuki-nishimura <julionadtoni7@yahoo.ne.jp>

--- components/modals/PostModal.js
+++ components/modals/PostModal.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import ReactModal from "react-modal";
 
 ReactModal.setAppElement("#__next");
 
 const PostModal = props => {
-  const { sentTo_1, sentTo_2, message } = props.post;
+  const { posts, idx } = props;
+  const [shownIdx, setShownIdx] = useState(idx);
+  const [random, setRandom] = useState(false);
+  const { sentTo_1, sentTo_2, message } = posts[shownIdx];
   const sentTo = sentTo_1 + sentTo_2;
+  console.log(shownIdx, posts);
+
+  const shiftPost = which => {
+    if (random) {
+      setShownIdx(Math.floor(Math.random()*posts.length));
+    } else {
+      //* 端から端もいける
+      setShownIdx(prevIdx => (posts.length+prevIdx+which)%posts.length)
+    }
+  }
 
   return (
     <div>
@@ -14,10 +28,6 @@ const PostModal = props => {
         closeTimeoutMS={300}
         onRequestClose={props.hideModal}
       >
-        <div style={container_styles}>
-          <p style={message_styles}>{message}</p>
-        </div>
-
         <span>
           <img
             src="/icons/cancel_icon.svg"
@@ -30,21 +40,49 @@ const PostModal = props => {
           </h3>
         </span>
 
+        <div style={container_styles}>
+          <p style={message_styles}>{message}</p>
+        </div>
+
       </ReactModal>
+
+      {/* post modal表示のときのみ表示 */}
+      <img
+        src="icons/shift-left.svg"
+        alt="前"
+        style={left_btn}
+        onClick={() => shiftPost(-1)}
+      />
+      <img
+        src={`icons/${(random) ? "random-active" : "random"}.svg`}
+        alt="ランダム"
+        style={random_btn}
+        onClick={() => setRandom(prevState => !prevState)}
+      />
+      <img
+        src="icons/shift-right.svg"
+        alt="次"
+        style={right_btn}
+        onClick={() => shiftPost(1)}
+      />
+
     </div>
+
   );
 }
 
 const modalStyle = {
   overlay: {
     position        : "fixed",
-    top             : 0,
-    left            : 0,
+    top             : "0",
+    left            : "0",
+    width           : '101%',
+    height          : '100%',
     backgroundColor : "rgba(50,50,50,0.8)",
   },
 
   content : {
-    top           : '20%',
+    top           : '10%',
     left          : '6%',
     padding       : '0',
     border        : '0',
@@ -73,6 +111,31 @@ const message_styles = {
   color      : '#7C7C7C',
   fontSize   : '26px',
   fontWeight : 'bold'
+}
+
+const btn_styles = {
+  display         : "block",
+  position        : "fixed",
+  top             : '80%',
+  width           : "60px",
+  height          : "60px",
+  zIndex          : "15000"
+}
+
+const left_btn = {
+  ...btn_styles, ...{
+    left : "10%"
+  }
+}
+const right_btn = {
+  ...btn_styles, ...{
+    right : "10%"
+  }
+}
+const random_btn = {
+  ...btn_styles, ...{
+    left : "calc(50% - 30px)"
+  }
 }
 
 export default PostModal;
